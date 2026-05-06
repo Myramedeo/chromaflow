3facc3688a6005b2f5831fd19be1aacfe3be4a6c
Roland Kim <103455333+Myramedeo@users.noreply.github.com>
2026-05-06 02:02:10 -0400

    Minor fixes to db for deployment

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,10 +6,17 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const connectionString = process.env.DIRECT_URL;
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
+}
 
 const pool = new Pool({
   connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 const adapter = new PrismaPg(pool);
